ef96f4d4348ccece061ec7c914ceffd352c24fcd
xiongtian <18771777589@163.com>
2019-08-21 18:25:56 +0800

    1.2 项目初始化

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component, Fragment } from 'react';
- class App extends Component { 
- render() { 
- return ( 
- <Fragment>App</Fragment> 
- );  
-} 
- } 
- export default App;
+	import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+	 class App extends Component { 
+	 render() { 
+	 return ( 
+	 <Fragment>
+	   <Button type="primary">primary</Button>
+	 </Fragment> 
+	 );  
+	} 
+	 } 
+	 export default App; 
