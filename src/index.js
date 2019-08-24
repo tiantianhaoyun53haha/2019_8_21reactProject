448857ed598e7d6071fa17900dd1efe993264fad
xiongtian <18771777589@163.com>
2019-08-24 20:57:04 +0800

    2.6 购物车--使用了redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import "./styles/iconfont.css";
+import   "./styles/index.css";
 import App from './App.js';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1 引入store
+import store from "./store";
+// 2 引入store和 react的连接器
+import {  Provider } from "react-redux";
+
+
+ReactDOM.render( <Provider store={store} ><App /></Provider>, document.getElementById('root'));
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,24 @@
 import React, { Component, Fragment } from 'react';
+import { Icon, NavBar, SwipeAction,Checkbox } from "antd-mobile";
+// 1 准备接受store的数据
+import { connect } from "react-redux";
  class Cart extends Component { 
  render() { 
  return ( 
- <Fragment>Cart</Fragment> 
+
  );  
 } 
  } 
- export default Cart;
+ 
+// 3 定义 store的数据 和 组件的props的映射对象
+// 这个对象类似 vue中 computed 
+const mapStateToProps = (state) => {
+
+    return {
+      carts: state.cartReducer.carts
+    }
+  }
+  
+  
+  // 2 开始连接
+  export default connect(mapStateToProps, null)(Cart); 
