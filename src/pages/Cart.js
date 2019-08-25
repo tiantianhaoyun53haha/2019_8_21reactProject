32c5e354f5efb4f635a5b9477a870be5e0ae29f7
xiongtian <18771777589@163.com>
2019-08-25 11:15:58 +0800

    2.7 购物车--静态样式完成

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,7 +5,63 @@ import { connect } from "react-redux";
  class Cart extends Component { 
  render() { 
  return ( 
-
+  <div className="cart">
+        {/* 导航栏 开始 */}
+        <NavBar
+          icon={<Icon type="left" />}
+          mode="dark"
+          onLeftClick={() => this.props.history.go(-1)}
+        >购物车</NavBar>
+        {/* 导航栏 结束 */}
+        {/* 购物车内容 开始 */}
+        <div className="cart_content">
+          {this.props.carts.map(v =>
+            <div key={v.id} className="cart_item">
+              <SwipeAction
+                style={{ backgroundColor: 'gray' }}
+                autoClose
+                right={[
+                  {
+                    text: 'Cancel',
+                    onPress: () => console.log('cancel'),
+                    style: { backgroundColor: '#ddd', color: 'white' },
+                  },
+                  {
+                    text: 'Delete',
+                    onPress: () => console.log('delete'),
+                    style: { backgroundColor: '#F4333C', color: 'white' },
+                  },
+                ]}
+                onOpen={() => console.log('global open')}
+                onClose={() => console.log('global close')}
+              >
+              <div className="cart_item_inner">
+                {/* 1 复选框 */}
+                <div className="chk_wrap">
+                <CheckboxItem />
+                </div>
+                {/* 2 图片 */}
+                <div className="goods_img_wrap">
+                  <img src={v.img_url} alt=""/>
+                </div>
+                {/* 3 商品名称+价格 */}
+                <div className="goods_info_wrap">
+                  <div className="goods_name">{v.title}</div>
+                  <div className="goods_price">￥{v.price}</div>
+                </div>
+                {/* 4 数量编辑工具 */}
+                <div className="goods_num_wrap">
+                  <div className="nums_operation">-</div>
+                  <div className="goods_num">{v.num}</div>
+                  <div className="nums_operation">+</div>
+                </div>
+                </div>
+              </SwipeAction>
+            </div>
+          )}
+        </div>
+        {/* 购物车内容 结束*/}
+      </div>
  );  
 } 
  } 
